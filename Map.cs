9ceb56d05583420bb79976c4d0a98e74cdd52833
Jacob <37407784+LeVausey@users.noreply.github.com>
2020-06-07 16:56:38 +0100

    Add files via upload
    Changes:  Added gizmos to start to draw the board map, cities and  connectors visually

--- Map.cs
+++ Map.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
 public class Map : MonoBehaviour
 {
-	public List<City> cities = new List<City>();
+    public List<City> cities = new List<City>();
+    public List<Connector> connectors = new List<Connector>();
 
-	public List<Connector> connectors = new List<Connector>();
+    public Transform city;
+    public float explosionRadius = 5.0f;
+    public static int boardSize = 10;
 
-	void Start()
+
+    void Start()
     {
         string filePath = @"C:\Users\Jacob\Documents\Masters\PowerGrid\PowerGrid\Assets\Scripts\Test.txt";
 
@@ -20,33 +27,33 @@ public class Map : MonoBehaviour
 
         foreach (var line in lines)
         {
-			//The entries
-			string[] entries = line.Split(',');
+            //The entries
+            string[] entries = line.Split(',');
 
-			//The number of cities in this line (entry count - 2)
-			int connectedCityCount = entries.Length - 2;
+            //The number of cities in this line (entry count - 2)
+            int connectedCityCount = entries.Length - 2;
 
-			//Make a new city for this line
-			City newCity = new City();
+            //Make a new city for this line
+            City newCity = new City();
 
             newCity.CityName = entries[0];
             newCity.SectorNum = entries[1];
 
-			for (int i = 2; i < connectedCityCount; i++)
-			{
-				//We're running from 2 (skip 0, and 1, because they're city name + sector number)
-				//because each entry here is a city which is connected to this one.
+            for (int i = 2; i < connectedCityCount; i++)
+            {
+                //We're running from 2 (skip 0, and 1, because they're city name + sector number)
+                //because each entry here is a city which is connected to this one.
 
-				//Make a new connection
-				Connector newConnector = new Connector();
-				newConnector.Connection = entries[i];
+                //Make a new connection
+                Connector newConnector = new Connector();
+                newConnector.Connection = entries[i];
 
-				//Add the connection
-				connectors.Add(newConnector);
-			}
+                //Add the connection
+                connectors.Add(newConnector);
+            }
 
 
-			cities.Add(newCity);
+            cities.Add(newCity);
         }
 
         foreach (var city in cities)
@@ -56,6 +63,17 @@ public class Map : MonoBehaviour
             Debug.Log($"{connector.Connection}");
 
         Console.ReadLine();
+    }
+
+    
+
+    void OnDrawGizmos()
+    {
+            // Draws a black line from this transform to the target
+            Gizmos.color = Color.black;
+            Gizmos.DrawLine(transform.position, city.position);
+            Gizmos.DrawWireSphere(transform.position, explosionRadius);
+            Gizmos.DrawWireCube(transform.position, new Vector2(boardSize * 10, boardSize * 0));
     }
 }
 
