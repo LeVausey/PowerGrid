1a7960ab4e97d08b1a25c7969468326fb4f9d1c1
Jacob <37407784+LeVausey@users.noreply.github.com>
2020-09-23 21:10:32 +0100

    Add files via upload

---
+++ Robots/RobotPicker.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotPickerOne
+{
+    public List<GameObject> head1;
+    public List<GameObject> torso1;
+    public List<GameObject> hip1;
+    public List<GameObject> legs1;
+    public List<GameObject> feet1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameObject randomPlayer = players[Random.Range(0, players.Count)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class RobotPickerTwo
+{
+
+    public List<GameObject> head2;
+    public List<GameObject> torso2;
+    public List<GameObject> hip2;
+    public List<GameObject> legs2;
+    public List<GameObject> feet2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         //GameObject randomPlayer = players[Random.Range(0, players.Count)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+public class RobotPickerThree
+{
+
+    public List<GameObject> head3;
+    public List<GameObject> torso3;
+    public List<GameObject> hip3;
+    public List<GameObject> legs3;
+    public List<GameObject> feet3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameObject randomPlayer = players[Random.Range(0, players.Count)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+public class RobotPickerFour
+{
+
+    public List<GameObject> head4;
+    public List<GameObject> torso4;
+    public List<GameObject> hip4;
+    public List<GameObject> legs4;
+    public List<GameObject> feet4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameObject randomPlayer = players[Random.Range(0, players.Count)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+public class RobotPickerFive
+{
+    public List<GameObject> head5;
+    public List<GameObject> torso5;
+    public List<GameObject> hip5;
+    public List<GameObject> legs5;
+    public List<GameObject> feet5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameObject randomPlayer = players[Random.Range(0, players.Count)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
