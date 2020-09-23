318ab354462e01dff132198f5d8b98a15c5320c2
Jacob <37407784+LeVausey@users.noreply.github.com>
2020-09-23 21:35:37 +0100

    Add files via upload
    Chnages: made small changes to Phase2 and Phase4, started to worked on Robot picker

--- Robots/Phase2Robot.cs
+++ Robots/Phase2Robot.cs
@@ -2,242 +2,242 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CheapestResource
-{
-    //Player Elektro 
-    public int playerElektro;
+//public class CheapestResource
+//{
+//    //Player Elektro 
+//    public int playerElektro;
 
-    //Player owned power plant cards
-    public List<Card> playerPowerPlants;
+//    //Player owned power plant cards
+//    public List<Card> playerPowerPlants;
 
-    /**
-   * choose a random card for this player
-   */
-    public Card PickCheapestResourceCard()
-    {
-        //Their cards
-        var playerCards = this.playerPowerPlants;
+//    /**
+//   * choose a random card for this player
+//   */
+//    public Card PickCheapestResourceCard()
+//    {
+//        //Their cards
+//        var playerCards = this.playerPowerPlants;
 
-        //Choose a random card
-        var randomCard = playerPowerPlants[Random.Range(0, playerCards.Count)];
+//        //Choose a random card
+//        var randomCard = playerPowerPlants[Random.Range(0, playerCards.Count)];
 
-        //And return this random card:
-        return randomCard;
-    }
+//        //And return this random card:
+//        return randomCard;
+//    }
 
-    public void BuyCard(PowerPlantCard powerPlantCard)
-    {
-        //They have enough money to buy the city: so add it to the list of owned cities
-        if (playerElektro >= powerPlantCard.faceValue)
-        {
-            //Add it to the list of owned cards
-            playerPowerPlants.Add(powerPlantCard);
+//    public void BuyCard(PowerPlantCard powerPlantCard)
+//    {
+//        //They have enough money to buy the city: so add it to the list of owned cities
+//        if (playerElektro >= powerPlantCard.faceValue)
+//        {
+//            //Add it to the list of owned cards
+//            playerPowerPlants.Add(powerPlantCard);
 
-            //Deduct that amount from their money
-            playerElektro -= powerPlantCard.faceValue;
-        }
-        else
-            UnityEngine.Debug.Log("you dont have enough money!");
-    }
+//            //Deduct that amount from their money
+//            playerElektro -= powerPlantCard.faceValue;
+//        }
+//        else
+//            UnityEngine.Debug.Log("you dont have enough money!");
+//    }
 
-}
+//}
 
-public class BuysTheFirstChoice
-{
-    //Player Elektro 
-    public int playerElektro;
+//public class BuysTheFirstChoice
+//{
+//    //Player Elektro 
+//    public int playerElektro;
 
-    //Player owned power plant cards
-    public List<Card> playerPowerPlants;
+//    //Player owned power plant cards
+//    public List<Card> playerPowerPlants;
 
-    /**
-   * choose a random card for this player
-   */
-    public Card PickRandomCard()
-    {
-        //Their cards
-        var playerCards = this.playerPowerPlants;
+//    /**
+//   * choose a random card for this player
+//   */
+//    public Card PickRandomCard()
+//    {
+//        //Their cards
+//        var playerCards = this.playerPowerPlants;
 
-        //Choose a random card
-        var randomCard = playerPowerPlants[Random.Range(0, playerCards.Count)];
+//        //Choose a random card
+//        var randomCard = playerPowerPlants[Random.Range(0, playerCards.Count)];
 
-        //And return this random card:
-        return randomCard;
-    }
+//        //And return this random card:
+//        return randomCard;
+//    }
 
-    public void BuyCard(PowerPlantCard powerPlantCard)
-    {
-        //They have enough money to buy the city: so add it to the list of owned cities
-        if (playerElektro >= powerPlantCard.faceValue)
-        {
-            //Add it to the list of owned cards
-            playerPowerPlants.Add(powerPlantCard);
+//    public void BuyCard(PowerPlantCard powerPlantCard)
+//    {
+//        //They have enough money to buy the city: so add it to the list of owned cities
+//        if (playerElektro >= powerPlantCard.faceValue)
+//        {
+//            //Add it to the list of owned cards
+//            playerPowerPlants.Add(powerPlantCard);
 
-            //Deduct that amount from their money
-            playerElektro -= powerPlantCard.faceValue;
-        }
-        else
-            UnityEngine.Debug.Log("you dont have enough money!");
-    }
+//            //Deduct that amount from their money
+//            playerElektro -= powerPlantCard.faceValue;
+//        }
+//        else
+//            UnityEngine.Debug.Log("you dont have enough money!");
+//    }
 
 
-}
+//}
 
-public class SupplyingMostCities
-{
-    //Player Elektro 
-    public int playerElektro;
+//public class SupplyingMostCities
+//{
+//    //Player Elektro 
+//    public int playerElektro;
 
-    //Player owned power plant cards
-    public List<Card> playerPowerPlants;
+//    //Player owned power plant cards
+//    public List<Card> playerPowerPlants;
 
-    /**
-   * choose a random card for this player
-   */
-    public Card PickRandomCard()
-    {
-        //Their cards
-        var playerCards = this.playerPowerPlants;
+//    /**
+//   * choose a random card for this player
+//   */
+//    public Card PickRandomCard()
+//    {
+//        //Their cards
+//        var playerCards = this.playerPowerPlants;
 
-        //Choose a random card
-        var randomCard = playerPowerPlants[Random.Range(0, playerCards.Count)];
+//        //Choose a random card
+//        var randomCard = playerPowerPlants[Random.Range(0, playerCards.Count)];
 
-        //And return this random card:
-        return randomCard;
-    }
+//        //And return this random card:
+//        return randomCard;
+//    }
 
-    public void BuyCard(PowerPlantCard powerPlantCard)
-    {
-        //They have enough money to buy the city: so add it to the list of owned cities
-        if (playerElektro >= powerPlantCard.faceValue)
-        {
-            //Add it to the list of owned cards
-            playerPowerPlants.Add(powerPlantCard);
+//    public void BuyCard(PowerPlantCard powerPlantCard)
+//    {
+//        //They have enough money to buy the city: so add it to the list of owned cities
+//        if (playerElektro >= powerPlantCard.faceValue)
+//        {
+//            //Add it to the list of owned cards
+//            playerPowerPlants.Add(powerPlantCard);
 
-            //Deduct that amount from their money
-            playerElektro -= powerPlantCard.faceValue;
-        }
-        else
-            UnityEngine.Debug.Log("you dont have enough money!");
-    }
-}
+//            //Deduct that amount from their money
+//            playerElektro -= powerPlantCard.faceValue;
+//        }
+//        else
+//            UnityEngine.Debug.Log("you dont have enough money!");
+//    }
+//}
 
-public class HighestNumber
-{
-    //Player Elektro 
-    public int playerElektro;
+//public class HighestNumber
+//{
+//    //Player Elektro 
+//    public int playerElektro;
 
-    //Player owned power plant cards
-    public List<Card> playerPowerPlants;
+//    //Player owned power plant cards
+//    public List<Card> playerPowerPlants;
 
-    /**
-   * choose a random card for this player
-   */
-    public Card PickRandomCard()
-    {
-        //Their cards
-        var playerCards = this.playerPowerPlants;
+//    /**
+//   * choose a random card for this player
+//   */
+//    public Card PickRandomCard()
+//    {
+//        //Their cards
+//        var playerCards = this.playerPowerPlants;
 
-        //Choose a random card
-        var randomCard = playerPowerPlants[Random.Range(0, playerCards.Count)];
+//        //Choose a random card
+//        var randomCard = playerPowerPlants[Random.Range(0, playerCards.Count)];
 
-        //And return this random card:
-        return randomCard;
-    }
+//        //And return this random card:
+//        return randomCard;
+//    }
 
-    public void BuyCard(PowerPlantCard powerPlantCard)
-    {
-        //They have enough money to buy the city: so add it to the list of owned cities
-        if (playerElektro >= powerPlantCard.faceValue)
-        {
-            //Add it to the list of owned cards
-            playerPowerPlants.Add(powerPlantCard);
+//    public void BuyCard(PowerPlantCard powerPlantCard)
+//    {
+//        //They have enough money to buy the city: so add it to the list of owned cities
+//        if (playerElektro >= powerPlantCard.faceValue)
+//        {
+//            //Add it to the list of owned cards
+//            playerPowerPlants.Add(powerPlantCard);
 
-            //Deduct that amount from their money
-            playerElektro -= powerPlantCard.faceValue;
-        }
-        else
-            UnityEngine.Debug.Log("you dont have enough money!");
-    }
+//            //Deduct that amount from their money
+//            playerElektro -= powerPlantCard.faceValue;
+//        }
+//        else
+//            UnityEngine.Debug.Log("you dont have enough money!");
+//    }
 
-}
+//}
 
-public class SecondSmallest
-{
-    //Player Elektro 
-    public int playerElektro;
+//public class SecondSmallest
+//{
+//    //Player Elektro 
+//    public int playerElektro;
 
-    //Player owned power plant cards
-    public List<Card> playerPowerPlants;
+//    //Player owned power plant cards
+//    public List<Card> playerPowerPlants;
 
-    /**
-   * choose a random card for this player
-   */
-    public Card PickRandomCard()
-    {
-        //Their cards
-        var playerCards = this.playerPowerPlants;
+//    /**
+//   * choose a random card for this player
+//   */
+//    public Card PickRandomCard()
+//    {
+//        //Their cards
+//        var playerCards = this.playerPowerPlants;
 
-        //Choose a random card
-        var randomCard = playerPowerPlants[Random.Range(0, playerCards.Count)];
+//        //Choose a random card
+//        var randomCard = playerPowerPlants[Random.Range(0, playerCards.Count)];
 
-        //And return this random card:
-        return randomCard;
-    }
+//        //And return this random card:
+//        return randomCard;
+//    }
 
-    public void BuyCard(PowerPlantCard powerPlantCard)
-    {
-        //They have enough money to buy the city: so add it to the list of owned cities
-        if (playerElektro >= powerPlantCard.faceValue)
-        {
-            //Add it to the list of owned cards
-            playerPowerPlants.Add(powerPlantCard);
+//    public void BuyCard(PowerPlantCard powerPlantCard)
+//    {
+//        //They have enough money to buy the city: so add it to the list of owned cities
+//        if (playerElektro >= powerPlantCard.faceValue)
+//        {
+//            //Add it to the list of owned cards
+//            playerPowerPlants.Add(powerPlantCard);
 
-            //Deduct that amount from their money
-            playerElektro -= powerPlantCard.faceValue;
-        }
-        else
-            UnityEngine.Debug.Log("you dont have enough money!");
-    }
+//            //Deduct that amount from their money
+//            playerElektro -= powerPlantCard.faceValue;
+//        }
+//        else
+//            UnityEngine.Debug.Log("you dont have enough money!");
+//    }
 
-}
+//}
 
-public class AllPowerPlants
-{
-    //Player Elektro 
-    public int playerElektro;
+//public class AllPowerPlants
+//{
+//    //Player Elektro 
+//    public int playerElektro;
 
-    //Player owned power plant cards
-    public List<Card> playerPowerPlants;
+//    //Player owned power plant cards
+//    public List<Card> playerPowerPlants;
 
-    /**
-   * choose a random card for this player
-   */
-    public Card PickRandomCard()
-    {
-        //Their cards
-        var playerCards = this.playerPowerPlants;
+//    /**
+//   * choose a random card for this player
+//   */
+//    public Card PickRandomCard()
+//    {
+//        //Their cards
+//        var playerCards = this.playerPowerPlants;
 
-        //Choose a random card
-        var randomCard = playerPowerPlants[Random.Range(0, playerCards.Count)];
+//        //Choose a random card
+//        var randomCard = playerPowerPlants[Random.Range(0, playerCards.Count)];
 
-        //And return this random card:
-        return randomCard;
-    }
+//        //And return this random card:
+//        return randomCard;
+//    }
 
-    public void BuyCard(PowerPlantCard powerPlantCard)
-    {
-        //They have enough money to buy the city: so add it to the list of owned cities
-        if (playerElektro >= powerPlantCard.faceValue)
-        {
-            //Add it to the list of owned cards
-            playerPowerPlants.Add(powerPlantCard);
+//    public void BuyCard(PowerPlantCard powerPlantCard)
+//    {
+//        //They have enough money to buy the city: so add it to the list of owned cities
+//        if (playerElektro >= powerPlantCard.faceValue)
+//        {
+//            //Add it to the list of owned cards
+//            playerPowerPlants.Add(powerPlantCard);
 
-            //Deduct that amount from their money
-            playerElektro -= powerPlantCard.faceValue;
-        }
-        else
-            UnityEngine.Debug.Log("you dont have enough money!");
-    }
+//            //Deduct that amount from their money
+//            playerElektro -= powerPlantCard.faceValue;
+//        }
+//        else
+//            UnityEngine.Debug.Log("you dont have enough money!");
+//    }
 
-}
+//}
--- Robots/Phase4Robot.cs
+++ Robots/Phase4Robot.cs
@@ -1,34 +1,102 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LastPlayer
 {
-   
+    public List<Player> players;
+    public List<City> playerCities;
+
 }
 
 public class AllCitiesNeverMoreThanFirst
 {
+    public List<Player> players;
+    public List<City> playerCities;
+     for (int i = players.Count; i > 0; i--)
+    //    {
+            
+    //        //Get the player at this index
+    //        var player = players[i];
 
+    ////Choose a random city
+    //var randomCity = player.FindRandomCity();
+
+    ////Buy the random city
+    //player.BuyCity(randomCity);
+           
+    //    }
 }
 
 public class SuppliedCities
 {
+    public List<Player> players;
+    public List<City> playerCities;
+    //    {
 
+    //        //Get the player at this index
+    //        var player = players[i];
+
+    ////Choose a random city
+    //var randomCity = player.FindRandomCity();
+
+    ////Buy the random city
+    //player.BuyCity(randomCity);
+
+    //    }
 }
 
 public class SixCities
 {
+    public List<Player> players;
+    public List<City> playerCities;
+    //    {
 
+    //        //Get the player at this index
+    //        var player = players[i];
+
+    ////Choose a random city
+    //var randomCity = player.FindRandomCity();
+
+    ////Buy the random city
+    //player.BuyCity(randomCity);
+
+    //    }
 }
 
 public class OneCityTwoCitiesThreeCities
 {
+    public List<Player> players;
+    public List<City> playerCities;
+    //    {
 
+    //        //Get the player at this index
+    //        var player = players[i];
+
+    ////Choose a random city
+    //var randomCity = player.FindRandomCity();
+
+    ////Buy the random city
+    //player.BuyCity(randomCity);
+
+    //    }
 }
 
 public class AllCities
 {
+    public List<Player> players;
+    public List<City> playerCities;
+    //    {
 
+    //        //Get the player at this index
+    //        var player = players[i];
+
+    ////Choose a random city
+    //var randomCity = player.FindRandomCity();
+
+    ////Buy the random city
+    //player.BuyCity(randomCity);
+
+    //    }
 }
 
